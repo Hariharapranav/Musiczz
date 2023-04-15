4ed0e77ae2459da22dc7b431dea7653377be6d7e
Hariharapranav <hpranav66@gmail.com>
2023-04-15 16:46:44 +0530

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,64 @@
+let allMusic = [
+  {
+    name: "Bae",
+    artist: "Anirudh Ravichander, Adithya R K",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Chellamma",
+    artist: "Anirudh Ravichander, Jonita Gandhi",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Donu Donu",
+    artist: "Alisha Thomas, Anirudh Ravichander",
+    img: "3",
+    src: "3"
+  },
+    {
+    name: "Engadi Porandha",
+    artist: "Anirudh Ravichander, Andrea Jeremiah",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Jolly O Gymkhana",
+    artist: "Vijay, Anirudh Ravichander",
+    img: "5",
+    src: "5"
+  },
+  {
+    name: "Kannazhaga",
+    artist: "Anirudh Ravichander",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "Life of Pazham",
+    artist: "Dhanush, Anirudh Ravichander",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "Mayakkama Kalakkama",
+    artist: "Anirudh Ravichander, Dhanush",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "Nee Partha",
+    artist: " Anirudh Ravichander, Vijay Yesudas and Shweta Mohan",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Osaka Osaka",
+    artist: "Pragathi Guruprasad, Anirudh Ravichander",
+    img: "10",
+    src: "10"
+  },
+  
+  
+];
